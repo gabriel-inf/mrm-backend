3d85c1603036bee5eacf5b6eee3a8b05f8191604
Jonathan Martins <jonathansilvamartinscanada@gmail.com>
2021-05-09 10:33:58 -0300

    Update stockItem routes

--- routes/item.route.js
+++ routes/item.route.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {create, findAll, findOne, deleteAll, deleteOne, 
-    update, sendToMaintenance, releaseFromMaintenance, registerLeave, registerArrive,
-    setReadyForRental} = require("../controllers/item.controller");
+    update} = require("../controllers/item.controller");
 
 // CRUD
 router.get("/", findAll);
@@ -11,12 +10,5 @@ router.post("/", create);
 router.delete("/:id", deleteOne);
 router.delete("/", deleteAll);
 router.put("/:id", update);
-
-//ACTIONS
-router.put("/rental/leave/:id", registerLeave);
-router.put("/rental/arrive/:id", registerArrive);
-router.put("/maintenance/send/:id", sendToMaintenance);
-router.put("/maintenance/release/:id", releaseFromMaintenance);
-router.put("/setReadyForRental/:id", setReadyForRental);
 
 module.exports = router;
