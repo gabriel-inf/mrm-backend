c6544db3fb465e99c91cc3547abd6bf37baed679
Jonathan Martins <jonathansilvamartinscanada@gmail.com>
2021-05-09 11:13:04 -0300

    Create rentContract Model

---
+++ models/rentcontract.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class RentContract extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.customer);
+    }
+  };
+
+  RentContract.init({
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE,
+    approvalDate: DataTypes.DATE,
+    paymentDueDate: DataTypes.DATE,
+    paidAt: DataTypes.DATE,
+    receiptUrl: DataTypes.STRING,
+    contractUrl: DataTypes.STRING,
+    durationMode: DataTypes.STRING,
+    value: DataTypes.DECIMAL,
+    status: DataTypes.STRING,
+    comment: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'rentContract',
+  });
+  return RentContract;
+};
