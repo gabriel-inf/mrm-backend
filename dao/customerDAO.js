f3b938ebb4a629ea69850388b1deb629ccd8087d
Gabriel Stepien <gabriel.stepien@inf.ufrgs.br>
2021-05-09 19:25:40 -0300

    Add days in debt query

--- dao/customerDAO.js
+++ dao/customerDAO.js
@@ -45,3 +45,37 @@ exports.getClientsAndDebts = async () => {
     return await executeSelect(query, []);
 }
 
+exports.getClientsAndTotalDaysInDebt = async () => {
+    const query = `
+        select
+            T."customerId",
+            sum(t.total_days) as contracts_and_aditives_total_days
+        from
+            (
+            select
+                rc."customerId",
+                DATE_PART('day', CURRENT_TIMESTAMP - rc."paymentDueDate") as total_days
+            from
+                "rentContracts" rc
+            where
+                rc."paidAt" is null
+                and rc."paymentDueDate" < CURRENT_TIMESTAMP
+        union all
+            select
+                rc."customerId",
+                DATE_PART('day', CURRENT_TIMESTAMP - a."paymentDueDate") as total_days
+            from
+                additives a
+            inner join "rentContracts" rc on
+                (rc.id = a."rentContractId")
+            where
+                a."paidAt" is null
+                and a."paymentDueDate" < CURRENT_TIMESTAMP ) T
+        group by
+            T."customerId"
+        order by
+            contracts_and_aditives_total_days desc
+    
+    `
+    return await executeSelect(query, []);
+}
