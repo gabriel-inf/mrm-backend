c7ce00082ebb7ecf006f23bdd51838b7e28e6a49
Jonathan Martins <jonathan@altiscale.com>
2021-05-16 11:35:46 -0300

    Add stockItemEvent  routes

---
+++ routes/stockitemevent.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const {create, findAll, findOne, deleteAll, deleteOne, update} = require("../controllers/stockitemevent.controller");
+
+router.get("/", findAll);
+router.get("/:id", findOne);
+router.post("/", create);
+router.delete("/:id", deleteOne);
+router.delete("/", deleteAll);
+router.put("/:id", update);
+
+module.exports = router;
