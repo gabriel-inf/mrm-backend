fbbe92e6e2d0bb07a399be699ca05857d93a40ae
Jonathan Martins <jonathan@altiscale.com>
2021-05-16 20:10:46 -0300

    Add basic query 2 to projetodedb routes

--- routes/projetodebd.route.js
+++ routes/projetodebd.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {getRentedStockItems} = require("../controllers/customer.controller");
+const {getActive} = require("../controllers/rentcontract.controller");
 
 router.get("/basicas/1/:id", getRentedStockItems);
+router.get("/basicas/2", getActive);
 
 module.exports = router;
